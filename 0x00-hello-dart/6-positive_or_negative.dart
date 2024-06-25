c438cc68f32d76450bb4302f680ec79fb1bb1176
omda3310 <6197@holbertonstudents.com>
2024-06-25 16:06:08 +0200

    positive or negative number

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  int number = int.parse(args[0]);
+  if (number > 0)
+    print('${number} is positive');
+  else if (number < 0)
+	  print('${number} is negative');
+	else
+	  print('${number} is zero');
+}

41fb81c66e5057314c84de247953ddfe354295e3
omda3310 <6197@holbertonstudents.com>
2024-06-27 00:51:12 +0200

    calculate area

--- 0x01-dart-function/5-calculate-area.dart
+++ 0x01-dart-function/5-calculate-area.dart
@@ -1,4 +1,4 @@
 double calculateArea(double height, double base) {
     double area = (height * base) / 2;
-    return area.parse(toStringAsFixed(2));
+    return double.parse(area.toStringAsFixed(2));
 }

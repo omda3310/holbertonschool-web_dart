d78f7435a67547dc0fd8e5690399039d8afb62a0
omda3310 <6197@holbertonstudents.com>
2024-06-27 19:07:23 +0200

    ste/get

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,37 @@
+class Password {
+  String password = '';
+	Password({
+		required this.password,
+	}) {
+      this._password = password;
+    }
+
+	String get password {
+		return _password;
+	}
+
+	void set password(String text) {
+		this._password = text;
+	}
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+    if (!RegExp(r'[A-Z]').hasMatch(password)) {
+      return false;
+    }
+    if (!RegExp(r'[a-z]').hasMatch(password)) {
+      return false;
+    }
+    if (!RegExp(r'[0-9]').hasMatch(password)) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}

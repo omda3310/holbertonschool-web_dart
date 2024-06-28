c52be24b962108ae889720778890e206991c6910
omda3310 <6197@holbertonstudents.com>
2024-06-28 17:38:38 +0200

    update

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,40 +1,53 @@
 import '6-password.dart';
 
 class User extends Password {
-	int? id;
-	String? name;
-	int? age;
-	double? height;
-	String user_password ='';
-	User({
-		this.id, this.name,
-		this.age, this.height,
-		String? user_password
-	}) : super(password: user_password ?? '') {
-		this.user_password = user_password ?? '';
-  	}
+  int? id;
+  String? name;
+  int? age;
+  double? height;
+  
+  User({
+    required int id,
+    required String name,
+    required int age,
+    required double height,
+    required String user_password}) : super(password: user_password ?? '') {
+	this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
 
-	Map toJson() {
-		return {
-			'id': '${this.id}',
-			'name': '${this.name}',
-			'age': '${this.age}',
-			'height': '${this.height}'
-		};
-	}
+  String get user_password {
+    return super.password;
+  }
 
-	static User fromJson(Map<dynamic, dynamic> userJson) {
-		return User(
-			name: userJson['name'],
-			age: userJson['age'],
-			height: userJson['height'],
-			id: userJson['id'],
-			user_password: userJson['user_password']
-		);
-	}
+  set user_password(String password) {
+    super.password = password;
+  }
 
-	@override
-	String toString() {
-		return "User(id: $id, name: $name, age: $age, height: $height, Password: ${Password(password: this.user_password).isValid()})";
-	}
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+      'user_password': this.user_password,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['user_password'],
+    );
+  }
+
+  @override
+  String toString() {
+    return "User(id: $id, name: $name, age: $age, height: $height, Password: ${isValid()})";
+  }
 }
--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,25 +1,21 @@
 class Password {
-	String _password = '';
+  String _password = '';
 
-	Password({password}) {
-		this._password = password ?? "";
-	}
+  Password({required String password}) : _password = password;
 
-	bool isValid() {
-		RegExp regExp = new RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$');
-		return regExp.hasMatch(this._password);
-	}
+  bool isValid() {
+    RegExp regExp = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$');
+    return regExp.hasMatch(_password);
+  }
 
-	String get password {
-		return this._password;
-	}
+  String get password => _password;
 
-	set password(String password) {
-		this._password = password;
-	}
+  set password(String password) {
+    _password = password;
+  }
 
-	@override
-	String toString() {
-		return 'Your Password is: ${this._password}';
-	}
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
 }

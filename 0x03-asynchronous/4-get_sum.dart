bd7333df26b46b057a7a95aed245d85f768690d8
omda3310 <6197@holbertonstudents.com>
2024-06-29 01:04:17 +0200

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import '4-util.dart';
+
+Future<double> calculateTotal() async {
+  try {
+    final userList = await fetchUserData();
+    final juser = jsonDecode(userList);
+    final userId = juser['id'];
+
+    final userOrders = await fetchUserOrders(userId);
+    final orders = jsonDecode(userOrders);
+
+    double total = 0;
+    for (var product in orders) {
+      final priceData = await fetchProductPrice(product);
+      final price = double.parse(jsonDecode(priceData).toString());
+      total += price;
+    }
+
+    return total;
+  } catch (err) {
+    return -1;
+  }
+}

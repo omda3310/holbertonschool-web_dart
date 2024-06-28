a11a8e99d225c5aa4f559d3379d072548b6c8eb9
omda3310 <6197@holbertonstudents.com>
2024-06-28 17:06:54 +0200

    initialize password

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,19 +1,36 @@
 import '6-password.dart';
 
 class User extends Password {
-	int id;
-	String name;
-	int age;
-	double height;
-	String user_password;
-	User({required this.id, required this.name, required this.age, required this.height, required this.user_password});
+	int? id;
+	String? name;
+	int? age;
+	double? height;
+	String user_password ='';
+	User({
+		this.id, this.name,
+		this.age, this.height,
+		String? user_password
+	}) : super(password: user_password ?? '') {
+		this.user_password = user_password ?? '';
+  	}
 
 	Map toJson() {
-		return {'id': '${this.id}', 'name': '${this.name}', 'age': '${this.age}', 'height': '${this.height}'};
+		return {
+			'id': '${this.id}',
+			'name': '${this.name}',
+			'age': '${this.age}',
+			'height': '${this.height}'
+		};
 	}
 
 	static User fromJson(Map<dynamic, dynamic> userJson) {
-		return User(name: userJson['name'], age: userJson['age'], height: userJson['height'], id: userJson['id'], user_password: userJson['user_password']);
+		return User(
+			name: userJson['name'],
+			age: userJson['age'],
+			height: userJson['height'],
+			id: userJson['id'],
+			user_password: userJson['user_password']
+		);
 	}
 
 	@override

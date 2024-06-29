9b43b2a0599fe724803dfada7366705550207574
omda3310 <6197@holbertonstudents.com>
2024-06-29 13:40:18 +0200

    Rick and Morty Characters

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  try {
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final characters = data['results'] as List;
+
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      print('Error: Unable to fetch characters. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error caught: $e');
+  }
+}

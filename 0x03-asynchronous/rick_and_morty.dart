a93afb88417cb29dd8447c7f9edd0edc1f6b076d
omda3310 <6197@holbertonstudents.com>
2024-06-29 18:05:58 +0200

    updated

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-printRmCharacters() async {
+Future<void> printRmCharacters() async {
   try {
     final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 

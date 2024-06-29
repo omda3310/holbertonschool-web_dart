530943a93f3bd657e0eb0f157936aaa9af2a1930
omda3310 <6197@holbertonstudents.com>
2024-06-29 14:04:24 +0200

    Rick and Morty Characters

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -2,20 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 printRmCharacters() async {
-  try {
-    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
-    
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final characters = data['results'] as List;
+  final response =
+      await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 
-      for (var character in characters) {
-        print(character['name']);
-      }
-    } else {
-      print('Error: Unable to fetch characters. Status code: ${response.statusCode}');
+  if (response.statusCode == 200) {
+    var data = jsonDecode(response.body);
+    List characters = data['results'];
+    for (var i = 0; i < 17 && i < characters.length; i++) {
+      print(characters[i]['name']);
     }
-  } catch (e) {
-    print('Error caught: $e');
+  } else {
+    throw Exception('Failed to load album');
   }
 }
